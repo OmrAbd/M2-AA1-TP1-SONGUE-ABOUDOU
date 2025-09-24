b9552c0b81e35c0c6596cdef0111ee2c18fc4f59
a21209699 <omar.aboudou@etu.univ-amu.fr>
2025-09-24 16:15:14 +0200

    Added new methods to the repository

--- src/main/java/fac/luminy/m2/aa1/tp1/repository/VoitureRepository.java
+++ src/main/java/fac/luminy/m2/aa1/tp1/repository/VoitureRepository.java
@@ -1,5 +1,6 @@
 package fac.luminy.m2.aa1.tp1.repository;
 
+import fac.luminy.m2.aa1.tp1.model.TypeVoiture;
 import fac.luminy.m2.aa1.tp1.model.entity.Voiture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +10,10 @@ import java.util.List;
 public interface VoitureRepository extends JpaRepository<Voiture, Long> {
 
     List<Voiture> findByProprietaireNom(@Param("nom") String nom);
+
+    List<Voiture> findVoitureByMarque(@Param("marque") String marque);
+
+    List<Voiture> findVoitureByType(@Param("type") TypeVoiture type);
+
+    List<Voiture> findVoitureByPrixBetween(double prixAfter, double prixBefore);
 }
