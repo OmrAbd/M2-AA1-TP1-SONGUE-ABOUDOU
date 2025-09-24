d160b2c1b20056040d0215250ef868e0f7cada2a
a21209699 <omar.aboudou@etu.univ-amu.fr>
2025-09-24 14:56:58 +0200

    Implemented recupererVoitureProprietaire

--- src/main/java/fac/luminy/m2/aa1/tp1/service/VoitureService.java
+++ src/main/java/fac/luminy/m2/aa1/tp1/service/VoitureService.java
@@ -1,8 +1,11 @@
 package fac.luminy.m2.aa1.tp1.service;
 
 import fac.luminy.m2.aa1.tp1.model.dto.VoitureDTO;
+import fac.luminy.m2.aa1.tp1.model.entity.Voiture;
+import fac.luminy.m2.aa1.tp1.repository.VoitureRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ import java.util.List;
 @AllArgsConstructor
 public class VoitureService {
 
+    @Autowired
+    VoitureRepository voitureRepository;
     /**
      * Récupère la liste des voitures pour un propriétaire donné.
      *
@@ -23,10 +28,13 @@ public class VoitureService {
     public List <VoitureDTO> recupererVoituresProprietaire(String nomProprietaire){
         log.info("Demande de recuperation des voitures pour le proprietaire avec le nom {}", nomProprietaire);
         List<VoitureDTO> listeRetour = new ArrayList<>();
+
+
         //Faire l'appel au repository pour recuperer la voiture a partir du nom du proprietaire
+        List <Voiture> listVoiture =  voitureRepository.findByProprietaireNom(nomProprietaire);
 
         // Convertir les voitures en voituresDTO
-
+        listVoiture.stream().map(VoitureDTO::new).forEach(listeRetour::add);
         // Retourner la liste des voitures
         log.info("{} voitures pour le proprietaire avec le nom {}",listeRetour.size(),nomProprietaire);
         return listeRetour;
