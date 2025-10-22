771c7e1420f98a28eda98b951a7bf4ae368ef3e1
a21209699 <omar.aboudou@etu.univ-amu.fr>
2025-10-22 11:41:56 +0200

    Added StartDate to Proprietaire renamed VendeurDTO.java en ProprietaireDTO.java

--- src/main/java/fac/luminy/m2/aa1/tp1/model/dto/ProprietaireDTO.java
+++ src/main/java/fac/luminy/m2/aa1/tp1/model/dto/ProprietaireDTO.java
@@ -3,15 +3,18 @@ package fac.luminy.m2.aa1.tp1.model.dto;
 import fac.luminy.m2.aa1.tp1.model.entity.Proprietaire;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
-public class VendeurDTO extends PersonneDTO{
+public class ProprietaireDTO extends PersonneDTO{
 
+    private LocalDate startDate;
     private List<VoitureDTO> voituresPossedees;
 
-    public VendeurDTO(Proprietaire proprietaire) {
+    public ProprietaireDTO(Proprietaire proprietaire) {
         super(proprietaire);
+        this.startDate = proprietaire.getStartDate();
         this.voituresPossedees = proprietaire.getVoituresPossedees() == null ? null :
                 proprietaire.getVoituresPossedees()
                         .stream()
--- src/main/java/fac/luminy/m2/aa1/tp1/model/entity/Proprietaire.java
+++ src/main/java/fac/luminy/m2/aa1/tp1/model/entity/Proprietaire.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity(name = "VENDEUR")
@@ -12,6 +13,8 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class Proprietaire extends Personne{
+
+    private LocalDate startDate;
 
     @OneToMany(mappedBy = "proprietaire", fetch = FetchType.LAZY)
     private List<Voiture> voituresPossedees = List.of();
